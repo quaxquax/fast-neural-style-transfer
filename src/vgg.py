a8cf5c21cfec6fd86bec9375548c475969300caa
hiepph <hoanghiepjp96@gmail.com>
2018-04-23 12:58:30 +0700

    vgg: implement vgg16 architecture

--- src/vgg.py
+++ src/vgg.py
@@ -1,24 +1,72 @@
-import torch
 import torch.nn as nn
 
 
 class VGG16(nn.Module):
-    def __init__(self):
-        pass
-        # Input (batch_size, width, height, channels)
+    def __init__(self, num_classes=1000):
+        super(VGG16, self).__init__()
+        self.features = nn.Sequential(
+            # 2 Conv2D 3x3 (stride 1), SAME, out 64, relu
+            # 1 MaxPool 2x2 (stride 2)
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
 
-        # 2 Conv2D 3x3 (stride 1), SAME, out 64, relu
-        # 1 MaxPool 2x2 (stride 2)nn
+            # 2 Conv2D 3x3 (stride 1), SAME, out 128, relu
+            # 1 MaxPool 2x2 (stride 2)
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
 
-        # 3 Conv2D 3x3 (stride 1), SAME, out 256, relu
-        # 1 Maxpool 2x2 (stride 2)
+            # 3 Conv2D 3x3 (stride 1), SAME, out 256, relu
+            # 1 Maxpool 2x2 (stride 2)
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
 
-        # 3 Conv2D 3x3 (stride 1), SAME, out 512, relu
-        # 1 Maxpool 2x2 (stride 2)
+            # 3 Conv2D 3x3 (stride 1), SAME, out 512, relu
+            # 1 Maxpool 2x2 (stride 2)
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
 
-        # 3 Conv2D 3x3 (stride 1), SAME, out 512, relu
-        # 1 Maxpool 2x2 (stride 2)
+            # 3 Conv2D 3x3 (stride 1), SAME, out 512, relu
+            # 1 Maxpool 2x2 (stride 2)
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
 
-        # Flatten
-        # 2 FC, out 4096, Dropout 0.5, relu
-        # 1 FC, out 1000, softmax
+        self.classifer = nn.Sequential(
+            # 2 FC 4096, relu, dropout 0.5
+            # 1 FC 1000 (aka num_classes of ImageNet)
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, num_classes)
+        )
+
+        def forward(self, x):
+            x = self.features(x)
+            # Flatten
+            x = x.view(x.size(0), -1)
+            x = self.classifer(x)
+            return x
