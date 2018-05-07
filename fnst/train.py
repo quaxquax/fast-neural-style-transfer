82b751499327752bb07ef3f73c5ec4eea8ce8e25
Hiep Pham <hiepph.2406@gmail.com>
2018-05-08 00:40:23 +0700

    feat(train): data pipeline

--- fnst/train.py
+++ fnst/train.py
@@ -1,9 +1,21 @@
 import argparse
 
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision import transforms
+
 
 def main(args):
     # DATA
     # Transform and Dataloader for COCO dataset
+    transform = transforms.Compose([
+        transforms.Resize(args.image_size),
+        # transforms.CenterCrop(args.image_size),
+        transforms.ToTensor(), # / 255.
+        transforms.Lambda(lambda x: x.mul(255))
+    ])
+    train_dataset = datasets.ImageFolder(args.dataset, transform)
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size)
 
     # MODEL
     # Define Image Transformation Network with MSE loss and Adam optimizer
@@ -26,5 +38,8 @@ if __name__ == '__main__':
                         help='Path to COCO dataset')
     parser.add_argument('--style', type=str, required=True,
                         help='Path to the style image')
+    parser.add_argument('--image-size', type=int, default=256,
+                        help='Size of training images')
+    parser.add_argument('--batch-size', type=int, default=4)
     args = parser.parse_args()
     main(args)
