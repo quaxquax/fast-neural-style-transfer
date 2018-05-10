48fb8350febda416d6dc86a0fb2ca2ff5b5d0494
hiepph <hiepph.2406@gmail.com>
2018-05-10 16:27:37 +0700

    feat: finish train pipeline

--- fnst/train.py
+++ fnst/train.py
@@ -1,6 +1,7 @@
 import argparse
 
 from PIL import Image
+from tqdm import tqdm
 import torch
 from torch import optim
 from torch import nn
@@ -48,7 +49,7 @@ def main(args):
     # Define Image Transformation Network with MSE loss and Adam optimizer
     transformer = TransformerNet().to(device)
     mse_loss = nn.MSELoss()
-    optimize = optim.Adam(transformer.parameters(), args.learning_rate)
+    optimizer = optim.Adam(transformer.parameters(), args.learning_rate)
 
     # Pretrained VGG
     vgg = VGG16(requires_grad=False).to(device)
@@ -71,10 +72,49 @@ def main(args):
     gram_style = [gram_matrix(y) for y in features_style]
 
     # TRAIN
-    # For each epoch:
-    # 1. Parse through Image Transformation Net. Then calculate content loss
-    # 2. Calculate style loss
-    # 3. Log these losses and regularly save model
+    for epoch in range(args.epochs):
+        transformer.train()
+        agg_content_loss = 0.
+        agg_style_loss = 0.
+
+        for batch_id, (x, _) in tqdm(enumerate(train_loader)):
+            n_batch = len(x)
+            optimizer.zero_grad()
+
+            # Parse throught Image Transformation network
+            y = transformer(x.to(device))
+            y = normalize_batch(y)
+            x = normalize_batch(x)
+
+            # Parse through VGG layers
+            features_y = vgg(y)
+            features_x = vgg(x)
+
+            # Calculate content loss
+            content_loss = args.content_weight * mse_loss(features_y.relu2_2, features_x.relu2_2)
+
+            # Calculate style loss
+            style_loss = 0.
+            for ft_y, gm_s in zip(features_y, gram_style):
+                gm_y = gram_matrix(ft_y)
+                style_loss += mse_loss(gm_y, gm_s[:n_batch, :, :])
+            style_loss *= args.style_weight
+
+            total_loss = content_loss + style_loss
+            total_loss.backward()
+            optimizer.step()
+
+            agg_content_loss += content_loss.item()
+            agg_style_loss += style_loss.item()
+
+            if (batch_id + 1) % args.log_interval == 0:
+                tqdm.write('[{}] ({})\t'
+                           'content: {:.6f}\t'
+                           'style: {:.6f}\t'
+                           'total: {:.6f}'.format(epoch, batch_id,
+                                                  agg_content_loss / (batch_id + 1),
+                                                  agg_style_loss / (batch_id + 1),
+                                                  (agg_content_loss + agg_style_loss) / (batch_id + 1)))
 
 
 if __name__ == '__main__':
@@ -83,9 +123,19 @@ if __name__ == '__main__':
                         help='Path to COCO dataset')
     parser.add_argument('--style', type=str, required=True,
                         help='Path to the style image')
+
     parser.add_argument('--image-size', type=int, default=256,
                         help='Size of training images')
+    parser.add_argument('--content-weight', type=float, default=1e5,
+                        help='Weight for content loss')
+    parser.add_argument('--style-weight', type=float, default=1e10,
+                        help='Weight fo style loss')
+
+    parser.add_argument('--epochs', type=int, default=2)
+    parser.add_argument('--log-interval', type=int, default=500)
     parser.add_argument('--batch-size', type=int, default=4)
     parser.add_argument('--learning-rate', type=float, default=1e-3)
+
     args = parser.parse_args()
+    print(args)
     main(args)
