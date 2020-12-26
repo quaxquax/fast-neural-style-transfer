cf6f91080160aad9a8a6731250c1b2fee29eadcc
quaxquax <henning.dekant@gmail.com>
2020-12-25 23:47:49 -0500

    Added some models

---
+++ checkpoints/lsMapping.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+mv bxphai.pth Sober.pth
+mv cubism.pth Golden.pth
+mv dnson.pth Lush_Color.pth
+mv fmarc.pth Bright_Abstract.pth
+mv glasmosaic.pth Glass_Mosaic.pth
+mv kadinsky_early.pth Technicolor_Vision.pth  
+mv kadinsky_faux.pth Collage.pth
+mv la_muse.pth Bold_Brush.pth
+mv plam.pth Red_Autumn.pth
+mv rain_princess.pth Sunset_Light.pth
+mv romantic.pth Romantic_Summer.pth
+mv trianglemosaic.pth Triangle_Mess.pth
+mv udnie.pth Solem_Abstraction.pth
+mv vangogh.pth Morning_Light.pth
+mv wave.pth Sun_Bleached.pth
